8e4ef0d579df4bd24a9951c0d1c018fc57ad8873
Jonathan Yu <yucjonathan@gmail.com>
2023-11-15 19:58:37 -0600

    grouped nodes/edges by type

--- perturbgraph.py
+++ perturbgraph.py
@@ -2,8 +2,12 @@ import argparse
 import json
 import numpy as np
 import math
+import warnings
+from enum import Enum
 from pydantic import BaseModel, Field, root_validator, Extra
-
+from collections import defaultdict
+from typing import Callable, TypeVar
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 class GraphsonObject(BaseModel):
     @root_validator(pre=True)
     def extract_values(cls, values):
@@ -12,15 +16,19 @@ class GraphsonObject(BaseModel):
             for key, value in values.items()
             }
     class Config:
-        extra = Extra.allow
+        extra = 'allow'
 
-
+class NodeType(Enum):
+    ProcessLet = "ProcessNode"
+    File = "FileNode"
+    IpChannel = "SocketChannelNode"
 class Node(GraphsonObject):
     id: int = Field(..., alias='_id')
-    node_type: str = Field(..., alias='TYPE')
+    type: NodeType = Field(..., alias='TYPE')
 
+edge_id_counter = 0
 class Edge(GraphsonObject):
-    id: int = Field(..., alias='_id')
+    id: int = Field(..., alias='_id') # TODO: May be edge ID conflicts when we export the graph
     srcid: int = Field(..., alias='_inV')
     dstid: int = Field(..., alias='_outV')
     optype: str = Field(alias='OPTYPE')
@@ -29,19 +37,41 @@ class Graph(BaseModel):
     nodes: list[Node] = Field(..., alias='vertices')
     edges: list[Edge] = Field(..., alias='edges')
     
+T = TypeVar('T')
+def group_by_lambda(
+        objects: list[GraphsonObject], 
+        get_attribute: Callable[[GraphsonObject], T]
+        ) -> dict[T, list[GraphsonObject]]:
+    grouped = {}
+    for object in objects:
+        key = get_attribute(object)
+        if not key in grouped:
+            grouped[key] = []
+        grouped[key].append(object)
 
+    return grouped
 
+    
 def main(args: dict) -> None:
     with open(args.input) as input_file:
         input_graph = json.load(input_file)
     
     graph = Graph(**input_graph)
+    node_lookup = {node.id: node for node in graph.nodes }
+    node_groups: dict[NodeType, Node] = group_by_lambda(graph.nodes, lambda node: node.type)
+
+    edge_lookup = {(edge.srcid, edge.dstid): edge for edge in graph.edges}
+    edge_groups: dict[str, Edge] = group_by_lambda(graph.edges, lambda edge: edge.optype)
+
+
     
+
+
+    
+def perturb(nodes: list[Node], edges: list[Edge]) -> (list[Node], list[Edge]):
     # https://web.archive.org/web/20170921192428id_/https://hal.inria.fr/hal-01179528/document
     epsilon_1: float
     epsilon_2 = 1
-    nodes: list(dict) = input_graph['nodes']
-    edges: list(dict) = input_graph['edges']
     n: int = len(nodes)
     m: int = len(edges)
 
@@ -53,8 +83,7 @@ def main(args: dict) -> None:
     else:
         theta = (1/epsilon_1) \
               * math.log(
-                  (n*(n-1)/(4*m_perturbed)) 
-                  + (1/2)*(math.exp(epsilon_1)-1)
+                  (n*(n-1)/(4*m_perturbed)) + (1/2)*(math.exp(epsilon_1)-1)
                 )
     n_1 = 0
 
