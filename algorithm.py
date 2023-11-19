c0e2f94cd79c04dc8d3c1b06b5d4965e2729252a
Jonathan Yu <yucjonathan@gmail.com>
2023-11-18 22:42:32 -0600

    feat: added stats to graphprocessor class

--- algorithm.py
+++ algorithm.py
@@ -8,38 +8,108 @@ from icecream import ic
 from graphson import Node, NodeType, Edge, EdgeType, Graph
 from utility import group_by_lambda, uniform_generator
 
-def perturb_graph(graph: Graph, epsilon_1: float) -> Graph:
-    node_groups = group_by_lambda(graph.nodes, lambda node: node.type)
-    edge_type_groups = group_by_lambda(graph.edges, lambda edge: EdgeType(edge, graph._node_lookup))
+PROCESSED = '#edges processed'
+SELF_REFERRING = '#self referring edges pruned'
+TIME_FILTERED = '#edges pruned by time'
 
-    new_edges: list[Edge] = []
-    for edge_type, edges in edge_type_groups.items():
-        perturbed_edges = extended_top_m_filter(
-            src_nodes=node_groups[edge_type.src_type],
-            dst_nodes=node_groups[edge_type.dst_type],
-            existing_edges=edges,
-            edge_type=edge_type,
-            epsilon_1=epsilon_1
-        )
-        new_edges.extend(perturbed_edges)
-
-    return Graph(vertices=graph.nodes, edges=new_edges)
-
-class Stats:
+class GraphProcessor:
     stats: dict[str, Counter[EdgeType,int]] = {}
-    def __init__(self, stats: list[str]):
-        for stat in stats:
+    def __init__(self):
+        for stat in [PROCESSED, SELF_REFERRING, TIME_FILTERED]:
             self.stats[stat] = Counter()
 
-    def increment(self, edge_type: EdgeType, stat: str) -> None:
+    def perturb_graph(self, input_graph: Graph, epsilon_1: float) -> Graph:
+        node_groups = group_by_lambda(input_graph.nodes, lambda node: node.type)
+        edge_type_groups = group_by_lambda(input_graph.edges, lambda edge: EdgeType(edge, input_graph._node_lookup))
+
+        new_edges: list[Edge] = []
+        for edge_type, edges in edge_type_groups.items():
+            perturbed_edges = self.extended_top_m_filter(
+                src_nodes=node_groups[edge_type.src_type],
+                dst_nodes=node_groups[edge_type.dst_type],
+                existing_edges=edges,
+                edge_type=edge_type,
+                epsilon_1=epsilon_1
+            )
+            new_edges.extend(perturbed_edges)
+
+        return Graph(vertices=input_graph.nodes, edges=new_edges)
+
+    def filter(self, 
+               src_node: Node, dst_node: Node, 
+               edge_type: EdgeType) -> bool:
+        if src_node == dst_node:
+            self.increment(SELF_REFERRING, edge_type)
+            return True
+        elif src_node.type == NodeType.PROCESS_LET \
+                and dst_node.type == NodeType.PROCESS_LET \
+                and src_node.time >= dst_node.time:
+            self.increment(TIME_FILTERED, edge_type)
+            return True
+        return False
+
+
+    def extended_top_m_filter(self, 
+                              src_nodes: list[Node], dst_nodes: list[Node], 
+                              existing_edges: list[Edge], 
+                              edge_type: EdgeType,
+                              epsilon_1: float, epsilon_2: float=1
+                              ) -> list[Edge]:
+        """Compute constants"""
+        n_s, n_d = len(src_nodes), len(dst_nodes)
+        m = len(existing_edges)
+        m_perturbed = m + int(np.round(np.random.laplace(0, 1.0/epsilon_2)))
+        if m_perturbed <= 0:
+            return []
+        
+        num_possible_edges = n_s*n_d
+        epsilon_t = math.log(
+            (num_possible_edges/m) - 1
+            )
+        if epsilon_1 < epsilon_t:
+            theta = epsilon_t / (2*epsilon_1)
+        else:
+            theta = math.log(
+                (num_possible_edges / (2*m_perturbed))
+                + (math.exp(epsilon_1)-1)/2
+            ) / epsilon_1
+
+        """Execute Extended Top-m Filter"""    
+        new_edges: set[Edge] = set()
+        for edge in existing_edges:
+            weight = 1 + np.random.laplace(0, 1.0/epsilon_1)
+            if weight > theta:
+                new_edges.add(edge)
+                
+        uniform_time = uniform_generator(existing_edges)
+        while len(new_edges) < m_perturbed:
+            src_node: Node = np.random.choice(src_nodes)
+            dst_node: Node = np.random.choice(dst_nodes)
+            self.increment(PROCESSED, edge_type)
+
+            # Provenance-specific constraints
+            # This filtering DEFINITELY affects our selection of theta
+            if self.filter(src_node, dst_node, edge_type):
+                continue
+            
+            new_edge = create_edge(src_node, dst_node, edge_type.optype, uniform_time)
+            if new_edge not in new_edges: # TODO: Also filter self-referential edges?
+                new_edges.add(new_edge)
+        return new_edges
+
+    def increment(self, stat: str, edge_type: EdgeType) -> None:
         self.stats[stat].update([str(edge_type)])
 
-PROCESSED = 'total processed'
-SELF_REFERRING = 'self referring'
-TIME_FILTERED = 'time filtered'
-etmf_stats = Stats([PROCESSED, SELF_REFERRING, TIME_FILTERED])
-
-
+    def get_stats_str(self) -> None:
+        lines = []
+        for stat, counter in self.stats.items():
+            lines.append(stat)
+            stat_str = {stat}
+            for edge_type, value in counter.items():
+                lines.append(f'  {edge_type}: {value}')
+            lines.append('')
+        return '\n'.join(lines)
+    
 def create_edge(src_node: Node, dst_node: Node,
                 optype: str,
                 time_func: Callable[[], int]):
@@ -50,55 +120,3 @@ def create_edge(src_node: Node, dst_node: Node,
         optype=optype,
         time=edge_time
     )
-def extended_top_m_filter(src_nodes: list[Node], dst_nodes: list[Node], 
-                          existing_edges: list[Edge], 
-                          edge_type: EdgeType,
-                          epsilon_1: float, epsilon_2: float=1
-                          ) -> list[Edge]:
-    """Compute constants"""
-    n_s, n_d = len(src_nodes), len(dst_nodes)
-    m = len(existing_edges)
-    m_perturbed = m + int(np.round(np.random.laplace(0, 1.0/epsilon_2)))
-    if m_perturbed <= 0:
-        return []
-    
-    num_possible_edges = n_s*n_d
-    epsilon_t = math.log(
-        (num_possible_edges/m) - 1
-        )
-    if epsilon_1 < epsilon_t:
-        theta = epsilon_t / (2*epsilon_1)
-    else:
-        theta = math.log(
-            (num_possible_edges / (2*m_perturbed))
-            + (math.exp(epsilon_1)-1)/2
-        ) / epsilon_1
-
-    """Execute Extended Top-m Filter"""    
-    new_edges: set[Edge] = set()
-    for edge in existing_edges:
-        weight = 1 + np.random.laplace(0, 1.0/epsilon_1)
-        if weight > theta:
-            new_edges.add(edge)
-            
-    uniform_time = uniform_generator(existing_edges)
-    while len(new_edges) < m_perturbed:
-        src_node: Node = np.random.choice(src_nodes)
-        dst_node: Node = np.random.choice(dst_nodes)
-        etmf_stats.increment(edge_type, PROCESSED)
-
-        # Provenance-specific constraints
-        # This filtering DEFINITELY affects our selection of theta
-        if src_node == dst_node:
-            etmf_stats.increment(edge_type, SELF_REFERRING)
-            continue
-        if src_node.type == NodeType.PROCESS_LET \
-            and dst_node.type == NodeType.PROCESS_LET \
-            and src_node.time >= dst_node.time:
-            etmf_stats.increment(edge_type, TIME_FILTERED)
-            continue
-        
-        new_edge = create_edge(src_node, dst_node, edge_type.optype, uniform_time)
-        if new_edge not in new_edges: # TODO: Also filter self-referential edges?
-            new_edges.add(new_edge)
-    return new_edges
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import shutil
 from collections import OrderedDict
@@ -10,7 +11,7 @@ import pandas as pd
 from icecream import ic
 from tqdm import tqdm
 
-from algorithm import perturb_graph, etmf_stats
+from algorithm import GraphProcessor
 from graphson import Graph
 from utility import save_dot, get_stats, count_disconnected_nodes
 
@@ -21,59 +22,64 @@ def evaluate(input_path: Path, output_dir: Path,
     results: list[pd.Series] = []
 
     configurations = [(input_path, output_dir, num_samples, epsilon) for epsilon in epsilons]
-    results = [evaluate_for_epsilon(*configuration)
-               for configuration in configurations ]
-    # with Pool(processes=8) as pool:
-    #     results = pool.starmap(evaluate_for_epsilon, configurations)
-    #     pool.close()
-    #     pool.join()
+    # results = [evaluate_for_epsilon(*configuration)
+    #            for configuration in configurations ]
+    with Pool(processes=8) as pool:
+        results = pool.starmap(evaluate_for_epsilon, configurations)
+        pool.close()
+        pool.join()
 
     return pd.concat(results, axis=1).T
 
 def evaluate_for_epsilon(input_path: Path, output_dir: Path, 
                          num_samples: int, epsilon_1: float
                          ) -> pd.Series:
+    graph_output_dir = output_dir / input_path.stem / f'epsilon-{epsilon_1}'
+    if os.path.isdir(graph_output_dir):
+        shutil.rmtree(graph_output_dir)
     metrics: dict[str, Callable[[Graph,Graph],float]] = {
         '#edges': lambda _, output_graph: len(output_graph.edges),
         '#edges kept': lambda input_graph, output_graph: len(set(input_graph.edges).intersection(output_graph.edges)),
         '#disconnected nodes': lambda _, output_graph: count_disconnected_nodes(output_graph)
     }
     metric_data = { key: [] for key, _ in metrics.items() }
+    processor = GraphProcessor()
     for i in tqdm(range(num_samples)):
         input_graph = Graph.load_file(input_path)
-        output_graph = perturb_graph(input_graph, epsilon_1=epsilon_1)
+        output_graph = processor.perturb_graph(input_graph, epsilon_1=epsilon_1)
 
         for metric, func in metrics.items():
             metric_data[metric].append(func(input_graph, output_graph))
 
         save_dot(output_graph.to_dot(), 
-                 output_dir / f'{input_path.stem}_epsilon-{epsilon_1}_{i}',
+                 graph_output_dir / f'{input_path.stem}_{i}',
                  dot=False, pdf=True)
 
+
+    with open(graph_output_dir / 'pruning-stats.txt', 'w', encoding='utf-8') as f:
+        f.write(processor.get_stats_str())
+        f.close()
 
     series_dict = OrderedDict()
     series_dict['epsilon'] = epsilon_1
     for metric, data in metric_data.items():
         for key, value in get_stats(metric, data).items():
             series_dict[key] = value
+
     return pd.Series(data=series_dict, index=series_dict.keys())
 
 
 def main(args: dict) -> None:
     original_graph = Graph.load_file(args.input_path)
-    if os.path.isdir(args.output_dir):
-        shutil.rmtree(args.output_dir)
     save_dot(original_graph.to_dot(), 
              Path('input') / args.input_path.stem, 
              pdf=True)
 
     evaluate(input_path     = args.input_path,
              output_dir     = args.output_dir,
-             num_samples    = 1,
-             epsilons       = [0.5, 1, 3, 5, 10, 15, 20, 30]
+             num_samples    = args.num_samples,
+             epsilons       = [ float(e) for e in args.epsilons.split(',') ]
             ).sort_values('epsilon').to_csv('output.csv', index=False)
-    
-    ic(etmf_stats.stats)
 
 
 if __name__ == '__main__':
@@ -82,8 +88,9 @@ if __name__ == '__main__':
                         required=True, help='Path to input graph')
     parser.add_argument('-o', '--output_dir', type=Path,
                         required=True, help='Path to output graph directory')
-    parser.add_argument('-n', '--num-graphs', type=int, 
-                        help='Number of perturbed graphs to generate')
-    parser.add_argument('-e', '--epsilon', type=float)
+    parser.add_argument('-n', '--num_samples', type=int, 
+                        required=True, help='Number of perturbed graphs to generate')
+    parser.add_argument('-e', '--epsilons', type=str,
+                        required=True, help='Comma separated list of float values for epsilon')
 
     main(parser.parse_args())
