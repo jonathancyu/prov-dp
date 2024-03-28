e362257206f2edc0293783f33a0de805d66c5e93
Jonathan Yu <yucjonathan@gmail.com>
2024-03-27 21:10:08 -0500

    fix: added more conditions to rglob, added safe cast for ref_id to int

--- source/algorithm/wrappers/edge.py
+++ source/algorithm/wrappers/edge.py
@@ -22,7 +22,7 @@ class Edge:
         self.edge.id = new_id
 
     def get_ref_id(self) -> int:
-        return int(self.edge.model_extra.get('REF_ID', '-1'))
+        return int(self.edge.model_extra['REF_ID'] or '-1')
 
     def get_src_id(self) -> int:
         return self.edge.src_id
