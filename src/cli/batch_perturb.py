14daa08efffff449039a9e91970b18a5c0787913
Jonathan Yu <yucjonathan@gmail.com>
2024-08-30 02:17:28 -0500

    feat: begrudgingly added beta/gamma configs

--- src/cli/batch_perturb.py
+++ src/cli/batch_perturb.py
@@ -10,39 +10,46 @@ class Configuration:
     epsilon: float
     delta: float
     alpha: float
+    beta: float
+    gamma: float
 
 
 def batch_run(args):
 
     configurations = [
-        # All values fixed (Table 4)
-        Configuration(epsilon=1, delta=0.5, alpha=0.5),
-        # Varying privacy budget (Table 4)
-        Configuration(epsilon=0.1, delta=0.5, alpha=0.5),
-        Configuration(epsilon=1, delta=0.5, alpha=0.5),
-        Configuration(epsilon=10, delta=0.5, alpha=0.5),
-        # Varying hyperparameters (Table 6)
-        # Varying alpha
-        Configuration(epsilon=1, delta=0.5, alpha=0.1),
-        Configuration(epsilon=1, delta=0.5, alpha=0.5),
-        Configuration(epsilon=1, delta=0.5, alpha=0.9),
-        # Varying delta
-        Configuration(epsilon=1, delta=0.1, alpha=0.5),
-        Configuration(epsilon=1, delta=0.5, alpha=0.5),
-        Configuration(epsilon=1, delta=0.9, alpha=0.5),
+# All values fixed (Table 4)
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+# Varying privacy budget (Table 4)
+Configuration(epsilon=0.1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=10, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+# Varying hyperparameters (Table 6)
+# Varying delta
+Configuration(epsilon=1, delta=0.1, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.9, alpha=0.5, beta=0.5, gamma=0.5),
+# Varying alpha
+Configuration(epsilon=1, delta=0.5, alpha=0.1, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.9, beta=0.5, gamma=0.5),
+# Varying beta
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.1, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.9, gamma=0.5),
+# Varying gamma
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.1),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.5),
+Configuration(epsilon=1, delta=0.5, alpha=0.5, beta=0.5, gamma=0.9),
     ]
 
     for config in configurations:
         epsilon_1 = config.epsilon * config.delta
         epsilon_2 = config.epsilon * (1 - config.delta)
         alpha = config.alpha
-        beta = 0
-        gamma = 0
+        beta = config.beta
+        gamma = config.gamma
 
         current_args = deepcopy(args)
-        print(
-            f"(0) beginning epsilon_1={epsilon_1}, alpha={alpha}, beta={beta}, gamma={gamma}"
-        )
         current_args.epsilon_1 = epsilon_1
         current_args.epsilon_2 = epsilon_2
         current_args.alpha = alpha
